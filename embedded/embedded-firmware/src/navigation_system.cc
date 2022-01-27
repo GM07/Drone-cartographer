7bae6fe685bbb7eb84e23171ede728cb62f49008
Félix-Antoine Constantin <felix-antoine.constantin@polymtl.ca>
2022-01-27 01:17:46 +0000

    La compilation fonctionne bien et on peut flasher le drone en C++

--- embedded/embedded-firmware/src/navigation_system.cc
+++ embedded/embedded-firmware/src/navigation_system.cc
@@ -3,22 +3,44 @@
 // TODO doit être enlevé lorsque le controlleur sera implémenté
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wreturn-type"
-#include <firmware_controller.h>
+#include "firmware_controller.h"
 #pragma GCC diagnostic pop
 
+extern "C" {
+#include "commander.h"
 #include "FreeRTOS.h"
-#include "crtp_commander_high_level.h"
 #include "task.h"
+}
 
 std::unique_ptr<bridge::AbstractController> getController() {
   return std::make_unique<bridge::FirmwareController>();
 }
 
+void setHoverSetpoint(setpoint_t *setpoint, float x, float y, float z,
+                      float yawrate) {
+  setpoint->mode.z = modeAbs;
+  setpoint->position.z = z;
+
+  setpoint->mode.yaw = modeVelocity;
+  setpoint->attitudeRate.yaw = yawrate;
+
+  setpoint->mode.x = modeVelocity;
+  setpoint->mode.y = modeVelocity;
+  setpoint->velocity.x = x;
+  setpoint->velocity.y = y;
+
+  setpoint->velocity_body = true;
+}
+
 extern "C" void appMain() {
   std::unique_ptr<bridge::AbstractController> controller = getController();
-  // crtpCommanderHighLevelTakeoff(1.0f, 5.0f);
-  while (true) {
-    vTaskDelay(M2T(5000));
-    // crtpCommanderHighLevelLand(0.0f, 5.0f);
+
+  vTaskDelay(M2T(3000));
+  setpoint_t point;
+  setHoverSetpoint(&point, 0.0f, 0.0f, 1.0f, 0.0f);
+
+  while (1) {
+    vTaskDelay(M2T(10));
+    commanderSetSetpoint(&point, 3);
   }
 }
