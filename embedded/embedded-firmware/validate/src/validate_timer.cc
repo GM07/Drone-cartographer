e3c16758f6e0d11b2358f0eb0940d2bedd87e232
Félix-Antoine Constantin <felix-antoine.constantin@polymtl.ca>
2022-02-15 02:14:47 +0000

    Added constant for random number

--- embedded/embedded-firmware/validate/src/validate_timer.cc
+++ embedded/embedded-firmware/validate/src/validate_timer.cc
@@ -7,8 +7,9 @@
 using ::testing::_;
 
 TEST(validateTimer, delayMSShouldCallVTaskDelay) {
+  const uint32_t kRandomTimerDelay = 25;
   mock = new FunctionsMock;
   EXPECT_CALL(*mock, vTaskDelay(_)).Times(1);
-  Timer::delayMs(25);
+  Timer::delayMs(kRandomTimerDelay);
   delete mock;
 }
