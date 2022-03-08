2c1ccf3c8d6ae3e8d564db4cc9d4d278fea8dff4
Nicholas Legrand <nicholas.legrand@polymtl.ca>
2022-03-08 15:19:39 -0500

    added required interfaces and enums for logs

--- frontend/src/utils/mission.ts
+++ frontend/src/utils/mission.ts
@@ -1,9 +1,29 @@
-export interface Point {
+export interface Point2d {
   x: number;
   y: number;
 }
 
-export interface DroneData {}
+export interface DroneSensors {
+  front: number;
+  back: number;
+  right: number;
+  left: number;
+}
+
+export enum DroneState {
+  IDLE = 0,
+  LANDING = 1,
+  EXPLORATION = 2,
+  TAKINGOFF = 3,
+  CRASHED = 4,
+}
+
+export interface DroneData {
+  sensors: DroneSensors;
+  position: Point2d;
+  battery_level: number;
+  state: DroneState;
+}
 
 export interface Logs {
   drone_info: [[DroneData]];
@@ -16,6 +36,6 @@ export interface Mission {
   number_of_drones: number;
   is_simulated: boolean;
   total_distance: number;
-  maps: [[Point]];
+  maps: [[Point2d]];
   logs: {};
 }
