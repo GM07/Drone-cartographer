c5c2dc14b40d3f69afcc683788859b858eea0fde
GM07 <mehgaya@gmail.com>
2022-03-11 18:10:23 +0000

    fix conflits

--- embedded/shared-firmware/src/vector3d.cc
+++ embedded/shared-firmware/src/vector3d.cc
@@ -41,7 +41,6 @@ float Vector3D::distanceTo(const Vector3D& other) const {
                    powf(m_z - other.m_z, 2));
 }
 
-<<<<<<< HEAD
 Vector3D Vector3D::reflect(const Vector3D& normal) const {
   return (*this) -
          normal.toUnitVector() * (*this * normal.toUnitVector()) * 2.0f;
@@ -57,8 +56,6 @@ float Vector3D::length() const {
   return static_cast<float>(sqrt(m_x * m_x + m_y * m_y + m_z * m_z));
 }
 
-=======
->>>>>>> dc58003b6332f7e28c995bb209548d3b885b8d47
 Vector3D& Vector3D::operator+=(const Vector3D& other) {
   m_x += other.m_x;
   m_y += other.m_y;
