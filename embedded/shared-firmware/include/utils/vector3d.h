48de99adf614843e920b838a58e060b8da4913f9
GM07 <mehgaya@gmail.com>
2022-03-11 17:59:06 +0000

    math

--- embedded/shared-firmware/include/utils/vector3d.h
+++ embedded/shared-firmware/include/utils/vector3d.h
@@ -1,23 +1,40 @@
 #ifndef VECTOR3D_H
 #define VECTOR3D_H
 
+#include <string>
+#include <utility>
+
+constexpr float kThreshold = 0.005f;
+
 class Vector3D {
  public:
-  Vector3D();
+  Vector3D() = default;
+  Vector3D(const Vector3D& other) = default;
+  Vector3D(Vector3D&& other) = default;
+  Vector3D& operator=(const Vector3D& other) = default;
+  Vector3D& operator=(Vector3D&& other) = default;
+
   Vector3D(float x, float y, float z);
+  virtual ~Vector3D() = default;
 
-  Vector3D operator+(const Vector3D& other);
-  Vector3D operator-(const Vector3D& other);
-  Vector3D operator*(const float factor);
-  Vector3D operator/(const float factor);
-  Vector3D operator=(const Vector3D& other);
+  Vector3D& operator+=(const Vector3D& other);
+  [[nodiscard]] Vector3D operator+(const Vector3D& other) const;
+  [[nodiscard]] Vector3D operator-(const Vector3D& other) const;
+  [[nodiscard]] Vector3D operator*(float factor) const;
+  [[nodiscard]] float operator*(const Vector3D& other) const;
+  [[nodiscard]] Vector3D operator/(float factor) const;
+  [[nodiscard]] bool operator==(const Vector3D& other) const;
+  [[nodiscard]] bool operator!=(const Vector3D& other) const;
 
-  float getX() const;
-  float getY() const;
-  float getZ() const;
+  [[nodiscard]] float distanceTo(const Vector3D& other) const;
+  [[nodiscard]] bool isAlmostEqual(const Vector3D& other,
+                                   float threshold = kThreshold) const;
+  [[nodiscard]] Vector3D reflect(const Vector3D& normal) const;
+  [[nodiscard]] Vector3D toUnitVector() const;
+  [[nodiscard]] float length() const;
+  std::string toString() const;
 
- private:
-  float m_x, m_y, m_z;
+  float m_x{0}, m_y{0}, m_z{0};
 };
 
 #endif
---
+++ embedded/shared-firmware/src/vector3d.cc
@@ -0,0 +1,69 @@
+#include "utils/vector3d.h"
+
+#include <cmath>
+
+Vector3D::Vector3D(float x, float y, float z) : m_x(x), m_y(y), m_z(z) {}
+
+Vector3D Vector3D::operator+(const Vector3D& other) const {
+  return Vector3D(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z);
+}
+
+Vector3D Vector3D::operator-(const Vector3D& other) const {
+  return Vector3D(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z);
+}
+
+Vector3D Vector3D::operator*(const float factor) const {
+  return Vector3D(m_x * factor, m_y * factor, m_z * factor);
+}
+
+float Vector3D::operator*(const Vector3D& other) const {
+  return m_x * other.m_x + m_y * other.m_y + m_z * other.m_z;
+}
+
+Vector3D Vector3D::operator/(const float factor) const {
+  return Vector3D(m_x / factor, m_y / factor, m_z / factor);
+}
+
+bool Vector3D::operator==(const Vector3D& other) const {
+  return m_x == other.m_x && m_y == other.m_y && m_z == other.m_z;
+}
+
+bool Vector3D::operator!=(const Vector3D& other) const {
+  return !(*this == other);
+}
+
+bool Vector3D::isAlmostEqual(const Vector3D& other, float threshold) const {
+  return distanceTo(other) <= threshold;
+}
+
+float Vector3D::distanceTo(const Vector3D& other) const {
+  return std::sqrt(powf(m_x - other.m_x, 2) + powf(m_y - other.m_y, 2) +
+                   powf(m_z - other.m_z, 2));
+}
+
+Vector3D Vector3D::reflect(const Vector3D& normal) const {
+  return (*this) -
+         normal.toUnitVector() * (*this * normal.toUnitVector()) * 2.0f;
+}
+
+Vector3D Vector3D::toUnitVector() const {
+  const float l = this->length();
+
+  return Vector3D(m_x / l, m_y / l, m_z / l);
+}
+
+float Vector3D::length() const {
+  return static_cast<float>(sqrt(m_x * m_x + m_y * m_y + m_z * m_z));
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D& other) {
+  m_x += other.m_x;
+  m_y += other.m_y;
+  m_z += other.m_z;
+  return *this;
+}
+
+std::string Vector3D::toString() const {
+  return "x: " + std::to_string(m_x) + " y: " + std::to_string(m_y) +
+         " z: " + std::to_string(m_z);
+}
