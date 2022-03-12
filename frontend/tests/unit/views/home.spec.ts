7ecbe4be58a4c4d887e3c211b15620e78d6b5dc9
Paul-André <>
2022-03-12 09:01:47 -0500

    Change folder

---
+++ frontend/tests/unit/views/home.spec.ts
@@ -0,0 +1,24 @@
+import Home from '@/views/home.vue';
+import {ROUTER} from '@/router';
+import {shallowMount, Wrapper} from '@vue/test-utils';
+import {DefaultProps} from 'vue/types/options';
+import {STORE_STUB} from '../stubs/store_stub';
+
+describe('home.vue', () => {
+  let wrapper: Wrapper<DefaultProps & Home, Element>;
+  let routerSpy: jasmine.Spy;
+
+  beforeEach(() => {
+    routerSpy = spyOn(ROUTER, 'push');
+    wrapper = shallowMount<Home>(Home, {store: STORE_STUB});
+  });
+
+  it('should create', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('changes path', () => {
+    wrapper.vm.configureMission();
+    expect(routerSpy).toHaveBeenCalledWith('/Mission');
+  });
+});
